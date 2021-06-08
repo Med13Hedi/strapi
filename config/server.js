0b4731536d773a01bf69133ddaa80e5cdf074bf7
unknown <medhedib@gmail.com>
2021-06-09 00:07:22 +0200

    your commit message

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c3f167350b69476727dd770f3483cb5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '35cdbeda-b6e4-48bb-b0d3-93aa9a4d3864'
+};
